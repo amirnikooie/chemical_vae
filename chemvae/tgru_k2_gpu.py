76f116aa605e0731b9a37b926c222e9c228ede47
Amir Nikooienejad <nikooienejad_amir@lilly.com>
2022-03-10 11:37:54 -0500

    updating files to make it compatible to tf version 2.x

--- chemvae/tgru_k2_gpu.py
+++ chemvae/tgru_k2_gpu.py
@@ -63,7 +63,7 @@ self.implementation ==0 : cpu
 '''
 
 ##### I'm sticking with CPU and thus the original code. For fully adapting v2 and gpu I will not use this code and probably move to LSTM or Attention based anywayself.
-def time_distributed_dense(x, w, b=None, dropout=None, use_bias=False,
+def time_distributed_dense(x, w, b=None, dropout=None,
                            input_dim=None, output_dim=None, timesteps=None):
     '''Apply y.w + b for every temporal slice y of x.
     '''
@@ -115,7 +115,7 @@ class TerminalGRU(GRU):
     # Implements professor forcing
 
     def __init__(self, units,
-                 temperature=1., rnd_seed=None, recurrent_dropout=0.0,
+                 temperature=1., rnd_seed=None, recurrent_dropout=0.0, use_bias=False,
                  **kwargs):
         # @param: temperature - sampling temperature
         # Annealing will be handled in the callbacks
@@ -128,7 +128,6 @@ class TerminalGRU(GRU):
         #self.units = units
         self.temperature = temperature
         self.rnd_seed = rnd_seed
-        self.use_bias = use_bias
         self.uses_learning_phase = True
         self.supports_masking = False
         #self.units = units
