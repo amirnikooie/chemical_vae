80e3559d8309d601bfafe10eba4ff404c477b9f9
Amir Nikooienejad <nikooienejad_amir@lilly.com>
2022-03-16 16:11:54 -0400

    updating files to make it compatible to tf version 2.x

--- chemvae/train_vae.py
+++ chemvae/train_vae.py
@@ -426,6 +426,7 @@ if __name__ == "__main__":
     print("All params:", params)
 
     tf.compat.v1.disable_eager_execution()
+    tf.compat.v1.experimental.output_all_intermediates(True)
     #tf.compat.v1.disable_v2_behavior()
 
     if params['do_prop_pred'] :
