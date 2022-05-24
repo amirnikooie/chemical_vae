06a6cf53ad38c95e71e1f75385a6120505428e41
Amir Nikooienejad <nikooienejad_amir@lilly.com>
2022-05-23 23:00:29 -0400

    some updates

--- chemvae/mol_utils.py
+++ chemvae/mol_utils.py
@@ -220,13 +220,15 @@ def hot_to_smiles(hot_x, alphabet, selfies): ## Changed to aplhabet. Work here..
     for x in hot_x:
         temp_str = ''
         for j in x:
+            sys.stdoutwrite(str(j)+"\n")
             index = np.argmax(j)
+            sys.stdoutwrite(str(index)+"***\n")
             temp_str += alphabet[index]
         temp_str = temp_str.replace(' ', '')
         if selfies:  # if SELFIES, decode to SMILES
             temp_str = sf.decoder(temp_str)
         smiles.append(temp_str)
-        sys.stdout.write(temp_str + "\n")
+        #sys.stdout.write(temp_str + "\n")
         sys.stdout.flush()
     return smiles
 
